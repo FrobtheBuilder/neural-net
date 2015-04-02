490a98f4f5bdc4dc337f37ed08512a922be90f4b
Frob the Builder <ebanyash@gmail.com>
2015-04-02 04:12:11 -0400

    I AM BECOME DEATH

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var p = new Architect.Perceptron(2, 20, 1)
+var p = new Architect.Perceptron(2, 90, 1)
 
 var rate = 0.3
 for (var i = 0; i < 20000; i++) {
@@ -20,39 +20,23 @@ var pJson = p.toJSON();
 document.querySelector("#container2").innerHTML = JSON.stringify(p.toJSON())
 
 var s = new sigma(container)
-/*
-s.graph.addNode({
-	id: "n0",
-	label: "hello",
-	x: 0,
-	y: 0,
-	size: 1,
-	color: "#f00"
-}).addNode({
-	id: "n1",
-	label: "world",
-	x: 1,
-	y: 1,
-	size: 1,
-	color: "#000"
-}).addEdge({
-	id: 'e0',
-	source: 'n0',
-	target: 'n1'
-})
-*/
 
-function netToGraph(s) {
+function netToGraph(JSONnetwork, sigma) {
+	var s = sigma
+	var pJson = JSONnetwork
+
 	var lastX = -1
 	var y = 0
 	pJson.neurons.forEach(function(e, i, arr) {
 		var x
-
+		var color = "#000"
 		if (e.layer == "input") {
 			x = 0
+			color = "#00d"
 		}
 		else if (e.layer == "output") {
 			x = 2
+			color = "#d00"
 		}
 		else {
 			x = Number(e.layer + 1)
@@ -70,7 +54,7 @@ function netToGraph(s) {
 			x: x,
 			y: y,
 			size: 1,
-			color: "#000"
+			color: color
 		})
 
 		lastX = x
@@ -96,7 +80,7 @@ function netToGraph(s) {
 	})
 }
 
-netToGraph(s)
+netToGraph(pJson, s)
 
 s.refresh()
 s.startForceAtlas2({
