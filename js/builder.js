396024e7bdd1f219ac788f4a94a4c5f4009e79b7
Frob the Builder <ebanyash@gmail.com>
2015-04-23 16:12:04 -0400

    Okay I think it's done

--- js/builder.js
+++ js/builder.js
@@ -1,42 +1,42 @@
 
 //takes a synaptic network in json format, injects it into a sigma graph
+Number.prototype.map = function ( in_min , in_max , out_min , out_max ) {
+  return ( this - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) + out_min;
+}
 function netToGraph(JSONnetwork, sigma) {
+	sigma.graph.clear()
 	var s = sigma
 	var pJson = JSONnetwork
 
-	var lastX = -1
-	var y = 0
+	var lastLayer = 0
+	var layer = 0
 	pJson.neurons.forEach(function(e, i) {
-		var x
 		var color = "#000"
+		var prevLayer = layer
 		if (e.layer == "input") {
-			x = 0
+			layer = 0
 			color = "#00d"
 		}
 		else if (e.layer == "output") {
-			x = 2
+			layer = lastLayer + 1
 			color = "#d00"
 		}
 		else {
-			x = Number(e.layer) + 1
+			layer = Number(e.layer) + 1
+			
 		}
-		if (x == lastX) {
-			y++
+		if (prevLayer != layer) {
+			lastLayer++
 		}
-		else {
-			y = 20
-		}
-
 		s.graph.addNode({
 			id: "neuron" + i,
-			label: String(i),
-			x: x,
-			y: y,
+			label: "L." + e.layer + ", B: " + Math.round(e.bias),
+			layer: layer,
+			x: 0,
+			y: 0,
 			size: 1,
 			color: color
 		})
-
-		lastX = x
 	})
 
 	pJson.connections.forEach(function(e, i, arr) {
@@ -54,8 +54,41 @@ function netToGraph(JSONnetwork, sigma) {
 			source: "neuron"+e.from,
 			target: "neuron"+e.to,
 			color: color,
-			size: weight*50
+			size: weight/2,
+			label: String(Math.round(e.weight))
 		})
 	})
 }
 
+function formatPerceptron(sigma, width, height) {
+	var layers = []
+	sigma.graph.nodes().forEach(function(e, i) {
+		if (layers[e.layer] === undefined) {
+			layers[e.layer] = []
+		}
+		layers[e.layer].push(e)
+	})
+	var maxY = 0;
+	layers.forEach(function(e, i, arr) {
+		e.forEach(function(le, li, larr) {
+			le.x = i.map(0, arr.length, 0, width)
+			le.y = li.map(0, larr.length, 0, height)
+			if (maxY < le.y)
+				maxY = le.y
+		})
+	})
+	
+	layers.forEach(function(e, i) {
+		e.forEach(function(le, li, larr) {
+			if (i == 0 || i == layers.length-1) {
+				if (larr.length == 1) {
+					le.y = maxY * (1/2)
+				}
+				else {
+					le.y = li.map(-1, larr.length, 0, maxY)
+				}
+			}
+		})
+	})
+	sigma.refresh()
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,56 @@
+var p
+
+var s = new sigma({
+	renderer: {                                                        
+        container: document.getElementById("container"),                                        
+        type: "canvas"
+    },                                                                 
+    settings: {
+		maxEdgeSize: 5,
+		labelThreshold: 2,
+		defaultEdgeLabelSize: 13,
+		edgeLabelSize: 'fixed',
+		edgeLabelThreshold: 0,
+	}
+})
+
+$(function() {
+	p = showPerceptron(perceptrons.XOR, s)
+	$("#xor").click(function() {
+		p = showPerceptron(train.XOR(new Architect.Perceptron(2, 10, 1)), s)
+	})
+	$("#xor-small").click(function() {
+		p = showPerceptron(perceptrons.XORSMALL, s)
+	})
+	$("#xor-dual").click(function() {
+		p = showPerceptron(perceptrons.XORDUAL, s)
+	})
+	$("#and").click(function() {
+		p = showPerceptron(perceptrons.AND, s)
+	})
+	$("#and-small").click(function() {
+		p = showPerceptron(perceptrons.ANDSMALL, s)
+	})
+	$("#sin").click(function() {
+		p = showPerceptron(perceptrons.SINSINGLE, s)
+	})
+	$("#sin-dual").click(function() {
+		p = showPerceptron(perceptrons.SINDUAL, s)
+	})
+	$("#dump").click(function() {
+		dumpSigmaJSON(s, $("#toilet"))
+		//$("#toilet").append($.csv.fromArrays(s.graph.nodes()))
+		//doCSV(JSON.stringify(s.graph.nodes()))
+	})
+})
+
+function noAct() {}
 
 function consolize(jelement) {
 	return function(message) {
 		jelement.append("<p>" + message + "</p>")
+		var objDiv = jelement.get([0])
+		objDiv.scrollTop = objDiv.scrollHeight
 	}
 }
 
@@ -11,35 +60,183 @@ var nlog = consolize($("#neural-console"))
 
 // a perceptron made by architect and a training function that takes one
 // parameter, the perceptron to train. Also a sigma instance.
-function showPerceptron(perceptron, sigma, train) {
-	train(perceptron);
-	netToGraph(perceptron.toJSON(), sigma) 
-	sigma.refresh()
-	
-	sigma.startForceAtlas2({
-	edgeWeightInfluence: 0.5,
-		outboundAttractionDistribution: true
-	})
-	
-	setTimeout(function() {
-		sigma.stopForceAtlas2()
-	}, 5000)
+function showPerceptron(p, sigma) {
+	perceptron = p()
+	perceptron.test()
+	netToGraph(perceptron.toJSON(), sigma)
+	formatPerceptron(sigma, 5, 5)
+	return perceptron;
 }
 
-var s = new sigma('container').settings({maxEdgeSize: 5})
-showPerceptron(new Architect.Perceptron(2, 90, 1), s, function(p) {
-	var rate = 0.3
-	for (var i = 0; i < 20000; i++) {
-		p.activate([0, 0])
-		p.propagate(rate, [0])
+function logBasicTests(log, str, net, options, realfn) {
+	log("<hr>Network trained. Sample results:")
+	if (realfn == null) realfn = function(){}
+	options.forEach(function(e, i) {
+		log(str + "(" + e + ") -> " + net.activate(e) + " <br>≈ " 
+				+ "expected: " + realfn(e))
+	})
+}
 
-		p.activate([0, 1])
-		p.propagate(rate, [1])
+var train = {
+	AND : function(p, r) {
+		var rate = (r || 0.3)
+			return function() {
+			for (var i = 0; i < 20000; i++) {
+				p.activate([0, 0])
+				p.propagate(rate, [0])
 
-		p.activate([1, 0])
-		p.propagate(rate, [1])
+				p.activate([0, 1])
+				p.propagate(rate, [0])
 
-		p.activate([1, 1])
-		p.propagate(rate, [0])
+				p.activate([1, 0])
+				p.propagate(rate, [0])
+
+				p.activate([1, 1])
+				p.propagate(rate, [1])
+			}
+			p.test = function() {
+				logBasicTests(nlog, "and", p, [
+					[0, 0],
+					[0, 1],
+					[1, 0],
+					[1, 1]
+				], function(x) {
+					if (x[0] == 1 && x[1] == 1) 
+						return 1
+					else
+						return 0
+				})
+			}
+			return p
+		}
+	},
+	XOR: function(p, r) {
+		var rate = (r || 0.3)
+		return function() {
+			for (var i = 0; i < 20000; i++) {
+				p.activate([0, 0])
+				p.propagate(rate, [0])
+
+				p.activate([0, 1])
+				p.propagate(rate, [1])
+
+				p.activate([1, 0])
+				p.propagate(rate, [1])
+
+				p.activate([1, 1])
+				p.propagate(rate, [0])
+			}
+			console.log(rate)
+			p.test = function() {
+				logBasicTests(nlog, "xor", p, [
+					[0, 0],
+					[0, 1],
+					[1, 0],
+					[1, 1]
+				], function(x){
+					if (x[0] != x[1]) {
+						return 1;
+					}
+					else {
+						return 0;
+					}
+				})
+			}
+			return p
+		}
+	},
+	SIN: function(p, r) {
+		var rate = (r || 0.2)
+		return function() {
+			var sinInputTable = _.range(0, 7, Math.PI/10)
+			var sinTable = sinInputTable.map(function(e, i) {
+				   return Math.sin(e)
+			   })
+
+			for (var i = 0; i < 100000; i++) {
+				sinTable.forEach(function(e, i) {
+					p.activate([sinInputTable[i]])
+					p.propagate(rate, [sinMap(e)])
+				})
+			}
+			p.test = function() {
+				logBasicTests(nlog, "sin", p, [
+					[Math.PI/4],
+					[Math.PI/3],
+					[Math.PI/2]
+				], Math.sin)
+				var points = _.range(0, 7, 0.2)
+				plot(
+					points,
+					points.map(function(e, i) {
+						return (sinUnmap(p.activate([e])[0]))
+					})
+				)
+			}
+			return p
+		}
 	}
-})
+}
+
+
+
+var perceptrons = {
+	XOR: train.XOR(new Architect.Perceptron(2, 10, 1)),
+	XORSMALL: train.XOR(new Architect.Perceptron(2, 3, 1)),
+	XORDUAL: train.XOR(new Architect.Perceptron(2, 10, 10, 1), 0.00001),
+	AND: train.AND(new Architect.Perceptron(2, 10, 1)),
+	ANDSMALL: train.AND(new Architect.Perceptron(2, 2, 1)),
+	SINSINGLE: train.SIN(new Architect.Perceptron(1, 20, 1), 0.1),
+	SINDUAL: train.SIN(new Architect.Perceptron(1, 10, 10, 1), 0.1)
+}
+
+//var p = showPerceptron(train.XOR(new Architect.Perceptron(2, 10, 1)), s)
+Chart.defaults.global.responsive = true;
+
+function plot(inputs, outputs) {
+	var contx = $("#plot").get(0).getContext("2d")
+	var plot = new Chart(contx).Line({
+		labels: inputs,
+		datasets: [
+			{
+				label: "sin(x) according to javascript",
+				fillColor: "rgba(220,220,220,0.0)",
+				strokeColor: "rgba(150,150,150,1)",
+				pointColor: "rgba(220,130,130,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: inputs.map(Math.sin)
+			},
+			{
+				label: "sin(x) according to perceptron",
+				fillColor: "rgba(190,190,190,0.0)",
+				strokeColor: "rgba(220,100,100,1)",
+				pointColor: "rgba(220,130,130,1)",
+				pointStrokeColor: "#fff",
+				pointHighlightFill: "#fff",
+				pointHighlightStroke: "rgba(220,220,220,1)",
+				data: outputs
+			}
+		]
+	}, { pointDot : false, bezierCurve : false })
+}
+
+function sinMap(x) {
+	return x.map(-1, 1, 0, 1)
+}
+
+function sinUnmap(x) {
+	return x.map(0, 1, -1, 1)
+}
+
+function dumpSigmaJSON(sigma, element) {
+	element.text("")
+	element.append("<p>NODES: <br>" + JSON.stringify(sigma.graph.nodes()) + "</p>")
+	element.append("<p>EDGES: <br>" + JSON.stringify(sigma.graph.edges()) + "</p>")
+}
+
+function dumpSigmaCSV(sigma, name) {
+	JSONToCSVConvertor(JSON.stringify(sigma.graph.nodes()), name + "nodes", true)
+	JSONToCSVConvertor(JSON.stringify(sigma.graph.edges()), name + "edges", true)
+}
